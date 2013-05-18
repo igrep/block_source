790afe6e8db21bb279e6e6f6a7cce29e8cce4fe2
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-05-18 15:33:34 +0900

    to begin with, try to use ripper

--- lib/block_source.rb
+++ lib/block_source.rb
@@ -1,5 +1,29 @@
 require "block_source/version"
+require 'ripper'
+require 'plock'
 
 module BlockSource
-  # Your code goes here...
+
+  class Parser < Ripper::Filter
+    def on_default event, token, data
+      p { [token, event] }
+      data << token
+    end
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  source_to_be_parsed = <<-EOF
+  p do
+    lam = lambda { 1 + 2 }
+    pro = proc do
+      puts 'hello, world'
+    end
+  end
+  EOF
+  b = BlockSource::Parser.new source_to_be_parsed
+  parsed_source = b.parse( '' )
+  puts parsed_source
+  p parsed_source == source_to_be_parsed
 end
