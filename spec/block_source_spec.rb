86e3cd19e6f4bd83f07639d4f09b720518521fc5
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-05-18 16:30:47 +0900

    add spec of the `block_source the only line in a line` feature

---
+++ spec/block_source_spec.rb
@@ -0,0 +1,9 @@
+describe BlockSource do
+  context 'when processing the only do-end block in a line' do
+    subject do # here must be newline so far!
+      proc_obj = proc do puts "hello, world" end
+      BlockSource.block_source proc_obj
+    end
+    it { should eq 'do puts "hello, world" end' }
+  end
+end
