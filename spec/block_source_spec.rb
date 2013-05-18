1765de53295a64aa4081e28a10ea2d9d156fc44e
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-05-18 17:09:03 +0900

    forgot to add higher describe block in the spec

--- spec/block_source_spec.rb
+++ spec/block_source_spec.rb
@@ -1,11 +1,13 @@
 require 'block_source'
 
 describe BlockSource do
-  context 'when processing the only do-end block in a line' do
-    subject do # here must be newline so far!
-      proc_obj = proc do puts "hello, world" end
-      BlockSource.block_source proc_obj
+  describe '.block_source' do
+    context 'when processing the only do-end block in a line' do
+      subject do # here must be newline so far!
+        proc_obj = proc do puts "hello, world" end
+        BlockSource.block_source proc_obj
+      end
+      it { should eq 'do puts "hello, world" end' }
     end
-    it { should eq 'do puts "hello, world" end' }
   end
 end
